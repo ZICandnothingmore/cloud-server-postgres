553892de5b27ba9b3631dc197c739f4c8c64f42c
ZICandnothingmore <baonhu0508@gmail.com>
2026-05-26 19:28:14 +0700

    update pool in auth.controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("./config/db");
+const pool = require("../config/db");
 
 const authController = require("../controllers/auth.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
